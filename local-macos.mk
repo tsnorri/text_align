8a02def1dfbc4ae98f631f757efe6c852be3886c
tsnorri <tsnorri@iki.fi>
2018-06-27 17:09:28 +0300

    Add -Wall -Werror

--- local-macos.mk
+++ local-macos.mk
@@ -6,6 +6,6 @@ CXX				= clang++-5.0
 GENGETOPT		= gengetopt
 PYTHON			= python3.6
 
-EXTRA_CXXFLAGS	= -mmacosx-version-min=10.11 -stdlib=libc++ -nostdinc++ -I/sw/include/c++/v1
+EXTRA_CXXFLAGS	= -mmacosx-version-min=10.11 -stdlib=libc++ -Wall -Werror -nostdinc++ -I/sw/include/c++/v1
 EXTRA_LDFLAGS	= -mmacosx-version-min=10.11 -stdlib=libc++ -L/sw/lib -Wl,-rpath,/sw/lib
-EXTRA_CFLAGS	= -mmacosx-version-min=10.11
+EXTRA_CFLAGS	= -mmacosx-version-min=10.11 -Wall -Werror
