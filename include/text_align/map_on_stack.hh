36908db0004519daf0e6844ea7e883e5d330708c
tsnorri <tsnorri@iki.fi>
2018-06-27 17:08:18 +0300

    Rename transform to map to clarify the intent

--- include/text_align/map_on_stack.hh
+++ include/text_align/map_on_stack.hh
@@ -12,12 +12,13 @@ namespace text_align { namespace detail {
 	template <int t_count>
 	struct map_on_stack
 	{
-		template <typename t_transform, typename t_fn, typename t_first, typename ... t_args>
-		void call(t_transform &transform, t_fn &fn, t_first &&first, t_args && ... args)
+		// Map the first argument and pass the result as the last one.
+		template <typename t_map, typename t_fn, typename t_first, typename ... t_args>
+		void call(t_map &map, t_fn &fn, t_first &&first, t_args && ... args)
 		{
-			transform(first, [&transform, &fn, &args...](auto &&transformed){
+			map(first, [&map, &fn, &args...](auto &&mapped){
 				map_on_stack <t_count - 1> ctx;
-				ctx.template call(transform, fn, args..., transformed);
+				ctx.template call(map, fn, args..., mapped);
 			});
 		}
 	};
@@ -26,8 +27,9 @@ namespace text_align { namespace detail {
 	template <>
 	struct map_on_stack <0>
 	{
-		template <typename t_transform, typename t_fn, typename ... t_args>
-		void call(t_transform &transform, t_fn &fn, t_args && ... args)
+		// All arguments have been mapped, call fn.
+		template <typename t_map, typename t_fn, typename ... t_args>
+		void call(t_map &map, t_fn &fn, t_args && ... args)
 		{
 			fn(args...);
 		}
@@ -37,24 +39,24 @@ namespace text_align { namespace detail {
 
 namespace text_align {
 	
-	// Instantiate t_transform t, then for each object o in t_args, call t(o).
+	// Instantiate t_map t, then for each object o in t_args, call t(o).
 	// Finally, call t_fn with the resulting objects.
-	template <typename t_transform, typename t_fn, typename ... t_args>
+	template <typename t_map, typename t_fn, typename ... t_args>
 	void map_on_stack_fn(t_fn &&fn, t_args && ... args)
 	{
-		t_transform transform;
+		t_map map;
 		detail::map_on_stack <sizeof...(t_args)> ctx;
-		ctx.template call(transform, fn, args...);
+		ctx.template call(map, fn, args...);
 	}
 	
 	
-	// Instantiate t_transform t and t_fn, then for each object o in t_args, call t(o).
+	// Instantiate t_map t and t_fn, then for each object o in t_args, call t(o).
 	// Finally, pass the objects to t_fn's operator().
-	template <typename t_transform, typename t_fn, typename ... t_args>
+	template <typename t_map, typename t_fn, typename ... t_args>
 	void map_on_stack(t_args && ... args)
 	{
 		t_fn fn;
-		map_on_stack_fn <t_transform>(fn, args...);
+		map_on_stack_fn <t_map>(fn, args...);
 	}
 }
 
