7bbda804cb4a820abfc634818be9f1c2723f76b1
Tuukka Norri <tsnorri@iki.fi>
2018-12-28 16:36:25 +0200

    Update the sample configuration

--- local-macos.mk
+++ local-macos.mk
@@ -1,11 +1,20 @@
-INCLUDE_DIRS	= /sw/opt/boost-1_67/include
-LIB_DIRS		= /sw/opt/boost-1_67/lib
+INCLUDE_DIRS		= /sw/opt/boost-1_67/include
+LIB_DIRS			= /sw/opt/boost-1_67/lib
 
-CC				= clang-5.0
-CXX				= clang++-5.0
-GENGETOPT		= gengetopt
-PYTHON			= python3.6
+CC					= clang-6.0
+CXX					= clang++-6.0
+#CC = gcc-7
+#CXX = g++-7
+PYTHON				= python3.6
 
-EXTRA_CXXFLAGS	= -mmacosx-version-min=10.11 -stdlib=libc++ -Wall -Werror -nostdinc++ -I/sw/include/c++/v1
-EXTRA_LDFLAGS	= -mmacosx-version-min=10.11 -stdlib=libc++ -L/sw/lib -Wl,-rpath,/sw/lib
-EXTRA_CFLAGS	= -mmacosx-version-min=10.11 -Wall -Werror
+WARNING_FLAGS		= -Wall -Werror -Wno-unused-const-variable -Wno-unused-function
+OPT_FLAGS			= -O0 -g
+
+SYSTEM_CFLAGS		= -mmacosx-version-min=10.11
+SYSTEM_CXXFLAGS		= -mmacosx-version-min=10.11 -stdlib=libc++ -nostdinc++ -I/sw/include/c++/v1
+SYSTEM_LDFLAGS		= -mmacosx-version-min=10.11 -stdlib=libc++ -L/sw/lib -Wl,-rpath,/sw/lib
+
+BOOST_ROOT			= /sw/opt/boost-1_67
+BOOST_INCLUDE		= -I$(BOOST_ROOT)/include
+BOOST_LIBS			= -L$(BOOST_ROOT)/lib -lboost_container-mt -lboost_iostreams-mt -lboost_serialization-mt -lboost_system-mt
+LIBDISPATCH_LIBS	= 
