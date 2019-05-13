25c12fca0eab83abe4f1a8c1c7d0a32c09c4cebd
tsnorri <tsnorri@iki.fi>
2019-05-13 22:05:38 +0300

    Remove default values of Make-defined variables

--- common.mk
+++ common.mk
@@ -2,8 +2,6 @@ INCLUDE_DIRS	?=
 LIB_DIRS		?=
 LIBRARIES		?=
 
-CC				?= cc
-CXX				?= c++
 GENGETOPT		?= gengetopt
 PYTHON			?= python
 PG_CONFIG		?= pg_config
@@ -21,10 +19,10 @@ PYTHON_LDFLAGS	?=
 INCLUDE_DIRS	+= ../include ../lib/libbio/include ../lib/libbio/lib/range-v3/include ../lib/libbio/lib/GSL/include
 LIBRARIES		+= boost_system-mt
 
-CPPFLAGS		= $(patsubst %,-I%,$(strip $(INCLUDE_DIRS)))
-CFLAGS			= $(WARNING_FLAGS) $(OPT_FLAGS) $(SYSTEM_CFLAGS) -std=c99
-CXXFLAGS		= $(WARNING_FLAGS) $(OPT_FLAGS) $(SYSTEM_CXXFLAGS) -std=c++17
-LDFLAGS			= $(WARNING_FLAGS) $(OPT_FLAGS) $(SYSTEM_LDFLAGS) -std=c++17 $(patsubst %,-L%,$(strip $(LIB_DIRS))) $(patsubst %,-l%,$(strip $(LIBRARIES)))
+CPPFLAGS		+= $(patsubst %,-I%,$(strip $(INCLUDE_DIRS)))
+CFLAGS			+= $(WARNING_FLAGS) $(OPT_FLAGS) $(SYSTEM_CFLAGS) -std=c99
+CXXFLAGS		+= $(WARNING_FLAGS) $(OPT_FLAGS) $(SYSTEM_CXXFLAGS) -std=c++17
+LDFLAGS			+= $(WARNING_FLAGS) $(OPT_FLAGS) $(SYSTEM_LDFLAGS) -std=c++17 $(patsubst %,-L%,$(strip $(LIB_DIRS))) $(patsubst %,-l%,$(strip $(LIBRARIES)))
 
 PYTHON_CFLAGS	+= $(SYSTEM_CFLAGS) -std=c++17
 PYTHON_CXXFLAGS	+= $(SYSTEM_CXXFLAGS) -std=c++17
