b6f89832b720719d8a2e1c277f4f9d14630cd902
tsnorri <tsnorri@iki.fi>
2019-05-13 22:07:21 +0300

    Update configuration, use Homebrew

--- local-macos.mk
+++ local-macos.mk
@@ -1,20 +1,22 @@
-INCLUDE_DIRS		= /sw/opt/boost-1_67/include
-LIB_DIRS			= /sw/opt/boost-1_67/lib
+CLANG_ROOT			= /usr/local/homebrew/opt/llvm
 
-CC					= clang-6.0
-CXX					= clang++-6.0
+INCLUDE_DIRS		= /usr/local/homebrew/opt/boost/include
+LIB_DIRS			= /usr/local/homebrew/opt/boost/lib
+
+CC					= /usr/local/homebrew/opt/llvm/bin/clang
+CXX					= /usr/local/homebrew/opt/llvm/bin/clang++
 #CC = gcc-7
 #CXX = g++-7
-PYTHON				= python3.6
+PYTHON				= python3.6m
 
 WARNING_FLAGS		= -Wall -Werror -Wno-unused-const-variable -Wno-unused-function
-OPT_FLAGS			= -O0 -g
+OPT_FLAGS			= -O2 -g
 
-SYSTEM_CFLAGS		= -mmacosx-version-min=10.11
-SYSTEM_CXXFLAGS		= -mmacosx-version-min=10.11 -stdlib=libc++ -nostdinc++ -I/sw/include/c++/v1
-SYSTEM_LDFLAGS		= -mmacosx-version-min=10.11 -stdlib=libc++ -L/sw/lib -Wl,-rpath,/sw/lib
+CPPFLAGS			= -D_LIBCPP_DISABLE_AVAILABILITY
+SYSTEM_CFLAGS		= -mmacosx-version-min=10.13
+SYSTEM_CXXFLAGS		= -mmacosx-version-min=10.13 -faligned-allocation -stdlib=libc++ -nostdinc++ -I$(CLANG_ROOT)/include/c++/v1
+SYSTEM_LDFLAGS		= -mmacosx-version-min=10.13 -faligned-allocation -stdlib=libc++ -L$(CLANG_ROOT)/lib -Wl,-rpath,$(CLANG_ROOT)/lib
 
-BOOST_ROOT			= /sw/opt/boost-1_67
+BOOST_ROOT			= /usr/local/homebrew/opt/boost
 BOOST_INCLUDE		= -I$(BOOST_ROOT)/include
 BOOST_LIBS			= -L$(BOOST_ROOT)/lib -lboost_container-mt -lboost_iostreams-mt -lboost_serialization-mt -lboost_system-mt
-LIBDISPATCH_LIBS	= 
